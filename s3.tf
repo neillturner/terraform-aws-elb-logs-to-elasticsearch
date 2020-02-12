a76c3e713e9cde357ab5cd52afbb03841351de31
Michael Gaber <Michael.Gaber@stroeer.de>
2020-02-12 13:13:22 +0100

    switch from interpolation to HCL2

--- s3.tf
+++ s3.tf
@@ -1,19 +1,19 @@
 resource "aws_s3_bucket_notification" "elb_logs_to_elasticsearch_vpc" {
-  count  = "${length(var.subnet_ids) > 0 ? 1 : 0}"
-  bucket = "${var.s3_bucket_id}"
+  count  = length(var.subnet_ids) > 0 ? 1 : 0
+  bucket = var.s3_bucket_id
 
   lambda_function {
-    lambda_function_arn = "${aws_lambda_function.elb_logs_to_elasticsearch_vpc[0].arn}"
+    lambda_function_arn = aws_lambda_function.elb_logs_to_elasticsearch_vpc[0].arn
     events              = ["s3:ObjectCreated:*"]
   }
 }
 
 resource "aws_s3_bucket_notification" "elb_logs_to_elasticsearch" {
-  count  = "${length(var.subnet_ids) == 0 ? 1 : 0}"
-  bucket = "${var.s3_bucket_id}"
+  count  = length(var.subnet_ids) == 0 ? 1 : 0
+  bucket = var.s3_bucket_id
 
   lambda_function {
-    lambda_function_arn = "${aws_lambda_function.elb_logs_to_elasticsearch[0].arn}"
+    lambda_function_arn = aws_lambda_function.elb_logs_to_elasticsearch[0].arn
     events              = ["s3:ObjectCreated:*"]
   }
 }
